b36a8df6956096ff5c6f512ba5a453a85ff0f7f8
iociveteres <29121162+iociveteres@users.noreply.github.com>
2025-09-17 20:55:34 +0300

    refactor: add dict template helper

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -35,8 +35,21 @@ func humanDate(t time.Time) string {
 	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
+func dict(values ...interface{}) map[string]interface{} {
+	m := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		k, ok := values[i].(string)
+		if !ok || i+1 >= len(values) {
+			continue
+		}
+		m[k] = values[i+1]
+	}
+	return m
+}
+
 var functions = template.FuncMap{
 	"humanDate": humanDate,
+	"dict":                    dict,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
