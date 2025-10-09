e989fd94ae44164f5fa364b8794f900c9e61bf42
iociveteres <29121162+iociveteres@users.noreply.github.com>
2025-10-10 00:54:49 +0300

    fix: set user to null on user deletion

--- migrations/000004_create_room_messages_table.up.sql
+++ migrations/000004_create_room_messages_table.up.sql
@@ -3,11 +3,11 @@ BEGIN;
 CREATE TABLE room_messages (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     room_id INTEGER NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
-    user_id INTEGER NOT NULL REFERENCES users(id),
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE SET NULL,
     message TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-create index idx_room_messages on room_messages(room_id, created_at)
+create index idx_room_messages on room_messages(room_id, created_at);
 
 COMMIT;
