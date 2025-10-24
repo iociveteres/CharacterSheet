2cb3551e0f6f013c799e0a4a3e13686e3b378726
Vladislav Sergeev <vladisser2ex4@gmail.com>
2025-10-24 15:43:37 +0300

    fix: add missing sessions migration

---
+++ migrations/000007_create_sessions_table.down.sql
@@ -0,0 +1,6 @@
+BEGIN;
+
+DROP INDEX IF EXISTS sessions_expiry_idx;
+DROP TABLE IF EXISTS sessions;
+
+END;
---
+++ migrations/000007_create_sessions_table.up.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+CREATE TABLE sessions (
+    token TEXT PRIMARY KEY,
+    data BYTEA NOT NULL,
+    expiry TIMESTAMPTZ NOT NULL
+);
+
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
+END;
